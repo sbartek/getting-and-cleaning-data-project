295e5a3bf55b5efd6c0a5454bb5b8817cbb287c3
sbartek <bartekskorulski@gmail.com>
2014-11-21 11:44:47 +0100

    finishes with aggregation

--- run_analysis.R
+++ run_analysis.R
@@ -1,39 +1,44 @@
 #data.table ???
-library(data.table)
+#library(data.table)
 
-#Download zip archive and unzip it into data folder
+#Downloads zip archive and unzip it into data folder
 if (!file.exists("data")) dir.create("data")
-
 zipURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 zipPath <- "data/project.zip"
 if (!file.exists(zipPath)) download.file(zipURL, destfile = zipPath, method = "curl")
 dataDir <- "data/UCI HAR Dataset"
 if (!file.exists(dataDir)) unzip(zipPath, exdir="data")
-list.files(dataDir)    
 
+#Reads features and activites
 featuresPath <- paste(dataDir, '/features.txt',sep="")
 features <- as.character(read.table(featuresPath)[,2])
-featuresMSNrs <- grep("(mean\\()|(std)", features, perl = T)
-#features[featuresMSNrs]
+
+#Prepare features for extracting only the measurements on the mean and
+#standard deviation.
+msInds <- grep("(mean\\()|(std)", features, perl = T)
+msFeats <- features[msInds]
+msFeats <- gsub("[-\\(\\)]", "", msFeats)
 
 activityPath <- paste(dataDir, '/activity_labels.txt',sep="")
 activity <- as.character(read.table(activityPath)[,2])
 
-#training data
+#Reads training data and orginizes it
 subTrPath <- paste(dataDir, '/train/subject_train.txt',sep="")
 subTr <- read.table(subTrPath)
 yTrPath <- paste(dataDir, '/train/y_train.txt',sep="")
 yTr <- read.table(yTrPath)
+#Uses descriptive activity names to name the activities in the data set
 yTr <- sapply(yTr, function(a) activity[a])
 
 xTrPath <- paste(dataDir, '/train/X_train.txt',sep="")
-xTr <- read.table(xTrPath)[,featuresMSNrs]
+# This will take a while:
+xTr <- read.table(xTrPath)[,msInds]
 
 tidyData <- cbind(subTr,yTr,xTr)
-colnames(tidyData) <- c("subject","activity",features[featuresMSNrs])
-head(tidyData)
+#Appropriately labels the data set with descriptive variable names. 
+colnames(tidyData) <- c("subject","activity",msFeats)
 
-#test data
+#Reads test data
 subTePath <- paste(dataDir, '/test/subject_test.txt',sep="")
 subTe <- read.table(subTePath)
 yTePath <- paste(dataDir, '/test/y_test.txt',sep="")
@@ -41,19 +46,24 @@ yTe <- read.table(yTePath)
 yTe <- sapply(yTe, function(a) activity[a])
 
 xTePath <- paste(dataDir, '/test/X_test.txt',sep="")
-xTe <- read.table(xTePath)[,featuresMSNrs]
+# This will take a while:
+xTe <- read.table(xTePath)[,msInds]
 
 tidyData2 <- cbind(subTe,yTe,xTe)
-colnames(tidyData2) <- c("subject","activity",features[featuresMSNrs])
+colnames(tidyData2) <- c("subject","activity",msFeats)
+
+# Merges the training and the test sets to create one data set.
 tidyData <- rbind(tidyData,tidyData2)
 
+# This writes them into 'tidyData.txt' file
+# It will take a while:
 write.table(tidyData, file = "data/tidyData.txt")
 
+#Creates a second, independent tidy data set with the average of each
+#variable for each activity and each subject.
 
-
-
-
-
-
-
-
+tidyData3 <- aggregate(
+    x =  tidyData[,msFeats],
+    by=list(subject=tidyData[,"subject"], activity=tidyData[,"activity"]),
+    FUN="mean")
+head(tidyData3[,c(1,2)])
